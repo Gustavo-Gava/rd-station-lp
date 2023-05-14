b1857974524dd64fc279bdf694ca4917e21fc102
Gustavo Gava <gustavogava123@hotmail.com>
2023-05-14 09:56:17 -0300

    feat: add Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import ReactModal from "react-modal";
 
 import * as S from "./styles";
+import { useState } from "react";
 
 export const Header = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
 	return (
 		<S.Container>
 			<S.Content>
@@ -10,10 +14,20 @@ export const Header = () => {
 					<Image src="/svg/logo-rd-station-default.svg" alt="Logo" width={136} height={25} />
 				</S.LogoWrapper>
 
-				<S.Menu>
+				<S.Menu onClick={() => setIsMenuOpen(!isMenuOpen)}>
 					<Image src="/svg/menu.svg" width={24} height={24} alt="List Icon" />
 				</S.Menu>
 			</S.Content>
+
+			<ReactModal
+				isOpen={isMenuOpen}
+				style={{
+					content: { inset: "65px 0 0 0", padding: 0, borderRadius: 0, height: "fit-content" },
+					overlay: { backgroundColor: "transparent", padding: 0 },
+				}}
+			>
+				<S.MenuContainer>Menu</S.MenuContainer>
+			</ReactModal>
 		</S.Container>
 	);
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,25 +1,29 @@
-
-import { applyContentContainerStyles, applyFullWidthContainerStyles } from '@/styles/utils'
-import styled from 'styled-components'
+import { applyContentContainerStyles, applyFullWidthContainerStyles } from "@/styles/utils";
+import styled from "styled-components";
 
 export const Container = styled.div`
-  ${applyFullWidthContainerStyles()};
-  display: flex;
-  padding: 20px 16px;
+	${applyFullWidthContainerStyles()};
+	position: relative;
+	display: flex;
+	padding: 20px 16px;
 
-  border-bottom: 1px solid ${({ theme }) => theme.colors.gray[30]};
-`
+	border-bottom: 1px solid ${({ theme }) => theme.colors.gray[30]};
+	z-index: 5;
+`;
 
 export const Content = styled.div`
-  ${applyContentContainerStyles()}
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
+	${applyContentContainerStyles()}
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
 
-export const LogoWrapper = styled.div`
-`
+export const LogoWrapper = styled.div``;
 
-export const Menu = styled.button`
+export const Menu = styled.button``;
 
-`
+export const MenuOverlay = styled.div``;
+
+export const MenuContainer = styled.div`
+	${applyFullWidthContainerStyles()};
+`;
