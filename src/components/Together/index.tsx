4e32b29577703078c4e5bd8b2cb5e1f09520a9f8
Gustavo Gava <gustavogava123@hotmail.com>
2023-05-14 09:56:55 -0300

    feat: add Together component

--- src/components/Together/index.tsx
+++ src/components/Together/index.tsx
@@ -4,17 +4,19 @@ export const Together = () => {
 	return (
 		<S.Container>
 			<S.Content>
-				<S.Title>Estaremos juntos desde o primeiro passo!</S.Title>
+				<S.TextWrapper>
+					<S.Title>Estaremos juntos desde o primeiro passo!</S.Title>
 
-				<S.Paragraph>
-					Se você ainda não sabe como extrair o máximo das ações digitais, não tem problema. O{" "}
-					<b>RD Station Marketin</b>g é mais que uma ferramenta, nos importamos com os seus
-					resultados.
-				</S.Paragraph>
+					<S.Paragraph>
+						Se você ainda não sabe como extrair o máximo das ações digitais, não tem problema. O{" "}
+						<b>RD Station Marketin</b>g é mais que uma ferramenta, nos importamos com os seus
+						resultados.
+					</S.Paragraph>
 
-				<S.Paragraph>
-					<b>Estaremos com você do planejamento até a prática!</b>
-				</S.Paragraph>
+					<S.Paragraph>
+						<b>Estaremos com você do planejamento até a prática!</b>
+					</S.Paragraph>
+				</S.TextWrapper>
 			</S.Content>
 		</S.Container>
 	);
--- src/components/Together/styles.ts
+++ src/components/Together/styles.ts
@@ -1,45 +1,67 @@
-import { applyContainerStyles, applyContentContainerStyles, applyFullWidthContainerStyles } from '@/styles/utils'
-import styled from 'styled-components'
+import {
+	applyContainerStyles,
+	applyContentContainerStyles,
+	applyFullWidthContainerStyles,
+} from "@/styles/utils";
+import styled from "styled-components";
 
 export const Container = styled.div`
-  ${applyFullWidthContainerStyles()}
+	${applyFullWidthContainerStyles()}
 
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  background-color: ${({ theme }) => theme.colors.primary.dark};
-  color: ${({ theme }) => theme.colors.system.background};
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	background-color: ${({ theme }) => theme.colors.primary.dark};
+	color: ${({ theme }) => theme.colors.system.background};
 
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    
-    width: 48px;
-    height: 48px;
-    clip-path: polygon(0 0, 100% 0, 0 100%);
+	&::before {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
 
-    background-color: ${({ theme }) => theme.colors.gray[10]};
-  }
-`
+		width: 48px;
+		height: 48px;
+		clip-path: polygon(0 0, 100% 0, 0 100%);
+
+		background-color: ${({ theme }) => theme.colors.gray[10]};
+	}
+`;
 
 export const Content = styled.div`
-  ${applyContentContainerStyles()}
+	${applyContentContainerStyles()}
+	width: 100%;
 
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-`
+	@media (${({ theme }) => theme.devices.notebook}) {
+		padding: 80px 0;
+	}
+`;
+
+export const TextWrapper = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+
+	@media (${({ theme }) => theme.devices.notebook}) {
+		width: 40%;
+		justify-content: flex-start;
+		align-items: flex-start;
+	}
+
+	@media (${({ theme }) => theme.devices.desktop}) {
+		width: 50%;
+	}
+`;
 
 export const Title = styled.h3`
-  font-size: ${({ theme }) => theme.fonts.size.heading.md};
-  line-height: ${({ theme }) => theme.fonts.lineHeight.heading.md};
-  font-weight: ${({ theme }) => theme.fonts.weight.bold};
-`
+	font-size: ${({ theme }) => theme.fonts.size.heading.md};
+	line-height: ${({ theme }) => theme.fonts.lineHeight.heading.md};
+	font-weight: ${({ theme }) => theme.fonts.weight.bold};
+`;
 
 export const Paragraph = styled.p`
-  font-size: ${({ theme }) => theme.fonts.size.body.md};
-  line-height: ${({ theme }) => theme.fonts.lineHeight.body.md};
-  font-weight: ${({ theme }) => theme.fonts.weight.normal};
-`
+	font-size: ${({ theme }) => theme.fonts.size.body.md};
+	line-height: ${({ theme }) => theme.fonts.lineHeight.body.md};
+	font-weight: ${({ theme }) => theme.fonts.weight.normal};
+`;
