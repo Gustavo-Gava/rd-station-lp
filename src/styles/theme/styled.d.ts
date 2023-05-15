a28e7ccfbcf1285e1bfc922bb676aec2b53c12a9
Gustavo Gava <gustavogava123@hotmail.com>
2023-05-15 10:19:10 -0300

    fix: removed unexistent type

--- src/styles/theme/styled.d.ts
+++ src/styles/theme/styled.d.ts
@@ -1,9 +1,8 @@
-import 'styled-components'
-import type theme from './theme'
+import "styled-components";
+import type theme from "./theme";
 
-declare module 'styled-components' {
-  type ThemeType = typeof theme
+declare module "styled-components" {
+	type ThemeType = typeof theme;
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends ThemeType {}
+	export interface DefaultTheme extends ThemeType {}
 }
